f4a95de321c95d3c829777d03da284249fd9361f
Esmeralda Nava-Valencia <141451860+xmeralda@users.noreply.github.com>
2023-11-15 15:01:38 -0800

    Initial version of the ui with ant design components

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,46 +1,139 @@
-import React from 'react';
-import logo from '../assets/logo.svg';
+import React, { useState } from 'react';
+import { ConfigProvider, theme, Button, Upload, message, Form, Input, Row, Col, Select, ColorPicker } from "antd";
+import { UploadOutlined } from '@ant-design/icons';
 import '../styles/ui.css';
 
+const { Option } = Select;
+
 function App() {
-  const textbox = React.useRef<HTMLInputElement>(undefined);
+  const { darkAlgorithm } = theme;
+  const [uploadedFile, setUploadedFile] = useState<File | null>(null);
+  const [lookAndFeel, setLookAndFeel] = useState<string>('');
+  const [primaryColor, setPrimaryColor] = useState('#5B8FF9');
+  const [secondaryColors, setSecondaryColors] = useState(['#99EF2B', '#EFDB2B', '#ED962C', '#EE2F32']);
+  const [style, setStyle] = useState<string>();
 
-  const countRef = React.useCallback((element: HTMLInputElement) => {
-    if (element) element.value = '5';
-    textbox.current = element;
-  }, []);
-
-  const onCreate = () => {
-    const count = parseInt(textbox.current.value, 10);
-    parent.postMessage({ pluginMessage: { type: 'create-rectangles', count } }, '*');
+  const handlePrimaryColorChange = (color: any) => {
+    setPrimaryColor(color.hex);
   };
 
-  const onCancel = () => {
-    parent.postMessage({ pluginMessage: { type: 'cancel' } }, '*');
+  const handleSecondaryColorChange = (color: any, index: number) => {
+    // TODO: Test to see if these colors are being updated
+    const newColors = [...secondaryColors];
+    newColors[index] = color.hex;
+    setSecondaryColors(newColors);
   };
 
-  React.useEffect(() => {
-    // This is how we read messages sent from the plugin controller
-    window.onmessage = (event) => {
-      const { type, message } = event.data.pluginMessage;
-      if (type === 'create-rectangles') {
-        console.log(`Figma Says: ${message}`);
+  const handleLookAndFeelChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setLookAndFeel(event.target.value);
+  };
+
+  const handleStyleChange = (value: string) => {
+    setStyle(value);
+  };
+
+  const handleGenerate = () => {
+    postImage();
+  }
+
+  const postImage = async () => {
+    if (!uploadedFile) {
+      message.error('No image selected');
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('image', uploadedFile);
+
+    try {
+      const response = await fetch('YOUR_API_ENDPOINT', {
+        method: 'POST',
+        body: formData,
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        message.success('Image uploaded successfully');
+        // Do something with the response if needed
+      } else {
+        message.error('Failed to upload image');
       }
-    };
-  }, []);
+    } catch (error) {
+      message.error('An error occurred while uploading the image');
+    }
+  }
+
+
+  const beforeUpload = (file: any) => {
+    const isImage = file.type.startsWith('image/');
+    if (!isImage) {
+      message.error('You can only upload image files!');
+    } else {
+      setUploadedFile(file); // Store the file
+    }
+    return isImage || Upload.LIST_IGNORE;
+  };
 
   return (
-    <div>
-      <img src={logo} />
-      <h2>Rectangle Creator</h2>
-      <p>
-        Count: <input ref={countRef} />
-      </p>
-      <button id="create" onClick={onCreate}>
-        Create
-      </button>
-      <button onClick={onCancel}>Cancel</button>
-    </div>
+      <ConfigProvider
+        theme={{
+          algorithm: darkAlgorithm,
+        }}>
+          <Upload
+            accept="image/*"
+            beforeUpload={beforeUpload}
+            listType="picture"
+            maxCount={1}
+            showUploadList={false}
+          >
+            <Button icon={<UploadOutlined rev={undefined} />}>Click to upload</Button>
+          </Upload>
+          <Form layout="vertical">
+            <Form.Item label="Look & Feel">
+              <Input placeholder="I want it to feel modern, slick..." value={lookAndFeel}
+                onChange={handleLookAndFeelChange}/>
+            </Form.Item>
+          </Form>
+          <Form>
+            <Row gutter={8} align="middle" style={{ alignItems: 'center' }}>
+              <Col style={{ display: 'flex', alignItems: 'center', justifyContent: 'end', paddingRight: '8px', paddingBottom:'10px' }}>
+                <span>Primary Color</span>
+              </Col>
+              <Col>
+                <Form.Item>
+                  <ColorPicker trigger="hover" defaultValue={primaryColor} onChange={(color) => handlePrimaryColorChange(color)}/>
+                </Form.Item>
+              </Col>
+            </Row>
+          </Form>
+          <Form>
+            <Row gutter={8} align="middle" style={{ alignItems: 'center' }}>
+              <Col style={{ display: 'flex', alignItems: 'center', justifyContent: 'end', paddingRight: '8px', paddingBottom:'10px' }}>
+                <span>Secondary Colors</span>
+              </Col>
+              <Col>
+                <Form.Item>
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                  {secondaryColors.map((color, index) => (
+                    <ColorPicker trigger="hover" style={{ marginRight: '1px' }} defaultValue={color} onChange={(color) => handleSecondaryColorChange(color, index)}/>
+                    ))}
+                </div>
+                </Form.Item>
+              </Col>
+            </Row>
+          </Form>
+          <Form layout='inline'>
+            <Form.Item label="Style">
+              <Select placeholder="Select a style" onChange={handleStyleChange} value={style}>
+                <Option value="tech">Cinematic</Option>
+                <Option value="nature">Anime</Option>
+                <Option value="business">Photographic</Option>
+                <Option value="business">Fantasy Art</Option>
+              </Select>
+            </Form.Item>
+          </Form>
+          <Button type='primary' size='large' block onClick={handleGenerate}>Generate</Button>
+      </ConfigProvider>      
   );
 }
 
