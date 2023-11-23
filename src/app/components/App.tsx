2b598041c2b0f14069c51776593e4b28a5228740
EdMasterNava <eduardonava2ms@gmail.com>
2023-11-22 22:12:51 -0800

    Color Picker Fix

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,9 +1,18 @@
-import React, { useState } from 'react';
-import { Buffer } from 'buffer';
-import { ConfigProvider, theme, Button, Form, Input, Row, Col, Select, ColorPicker } from "antd";
-
-import axios from 'axios';
+import React, { useRef, useState } from 'react';
+import { 
+  ConfigProvider, 
+  theme, 
+  Button, 
+  Form, 
+  Input, 
+  Row, 
+  Col, 
+  Select, 
+  ColorPicker 
+} from "antd";
 import { UploadOutlined } from '@ant-design/icons';
+import { Buffer } from 'buffer';
+import axios from 'axios';
 import '../styles/ui.css';
 
 const { TextArea } = Input;
@@ -16,7 +25,8 @@ function App() {
   const [secondaryColors, setSecondaryColors] = useState(['#99EF2B', '#EFDB2B', '#ED962C', '#EE2F32']);
   const [style, setStyle] = useState<string>('');
   const [selectedImage, setSelectedImage] = useState(null);
-  // const [generatedImage, setGeneratedImage] = useState(null)
+  const [loading, setLoading] = useState(false);
+  const fileInputRef = useRef(null);
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
@@ -39,13 +49,13 @@ function App() {
   };
 
   const handlePrimaryColorChange = (color: any) => {
-    setPrimaryColor(color.hex);
+    setPrimaryColor(color.toHexString());
   };
 
   const handleSecondaryColorChange = (color: any, index: number) => {
-    // TODO: Test to see if these colors are being updated
     const newColors = [...secondaryColors];
-    newColors[index] = color.hex;
+    newColors[index] = color.toHexString();
+    console.log(newColors)
     setSecondaryColors(newColors);
   };
 
@@ -59,28 +69,29 @@ function App() {
 
   const handleGenerate = () => {
     postImage();
-  }
-  function base64ToUint8Array(base64String: string): Uint8Array | null {
+  };
+
+  const base64ToUint8Array = (base64String: string): Uint8Array | null => {
     try {
-      console.log("base64ToUint8Array: ", base64String)
       // Remove the data URL prefix if present
       const base64WithoutPrefix = base64String.replace(/^data:\w+\/\w+;base64,/, '');
   
       // Decode the base64 string using Buffer
-      // const buffer = Buffer.from(base64WithoutPrefix, 'base64');
       const buffer = Buffer.from(base64WithoutPrefix, 'base64');
   
       // Create a Uint8Array from the Buffer
       const uint8Array = new Uint8Array(buffer);
   
       return uint8Array;
-    } catch (error) {
+    } 
+    catch (error) {
       console.error('Error converting base64 to Uint8Array:', error.message);
       return null;
     }
-  }
+  };
 
   const postImage = async () => {
+    setLoading(true);
     try {
       axios.post('http://127.0.0.1:5000/generate', 
         {
@@ -90,13 +101,10 @@ function App() {
           secondaryColors,
           style
         }
-      )
-      .then(response => {
+      ).then(response => {
         const b64JSON = response.data.result
         const bs4 = JSON.parse(b64JSON)["b64"]
         const uint8Array = base64ToUint8Array(bs4)
-        
-
         parent.postMessage({ pluginMessage: { type: 'generated-image', uint8Array } }, '*');
       })
       .catch(error => {
@@ -105,79 +113,134 @@ function App() {
     } catch (error) {
       console.log(error);
     }
-  }
-
+    setLoading(false);
+  };
 
   const triggerFileInput = () => {
     fileInputRef.current.click();
   };
+
   return (
       <ConfigProvider
         theme={{
           algorithm: darkAlgorithm,
-        }}>
-          <div style={{marginBottom:40}}>
-            <input
-              type="file"
-              accept="image/*"
-              style={{ display: 'none' }}
-              onChange={handleImageChange}
-              ref={fileInputRef}
-            />
-            <Button type='dashed' size='large' icon={<UploadOutlined rev={undefined}/>} onClick={triggerFileInput}>
-              Upload Image
-            </Button>
-          </div>
-          <Form layout="vertical">
-            <Form.Item label="Look & Feel">
-            <TextArea 
-            placeholder="UI that feels fun and exciting..." 
-            value={lookAndFeel}
-            onChange={handleLookAndFeelChange}
-            autoSize={{ minRows: 3, maxRows: 3 }}  // Set min and max rows to 3
+        }}
+      >
+        <div style={{marginBottom:40}}>
+          <input
+            type="file"
+            accept="image/*"
+            style={{ display: 'none' }}
+            onChange={handleImageChange}
+            ref={fileInputRef}
           />
-            </Form.Item>
-          </Form>
-          <Form>
-            <Row gutter={8} align="middle" style={{ alignItems: 'center' }}>
-              <Col style={{ display: 'flex', alignItems: 'center', justifyContent: 'end', paddingRight: '8px', paddingBottom:'10px' }}>
-                <span>Primary Color</span>
-              </Col>
-              <Col>
-                <Form.Item>
-                  <ColorPicker trigger="hover" placement='bottom' defaultValue={primaryColor} onChange={(color) => handlePrimaryColorChange(color)}/>
-                </Form.Item>
-              </Col>
-            </Row>
-          </Form>
-          <Form>
-            <Row gutter={8} align="middle" style={{ alignItems: 'center' }}>
-              <Col style={{ display: 'flex', alignItems: 'center', justifyContent: 'end', paddingRight: '8px', paddingBottom:'10px' }}>
-                <span>Secondary Colors</span>
-              </Col>
-              <Col>
-                <Form.Item>
-                <div style={{ display: 'flex', alignItems: 'center' }}>
-                  {secondaryColors.map((color, index) => (
-                    <ColorPicker trigger="hover" placement='bottom' style={{ marginRight: '1px' }} defaultValue={color} onChange={(color) => handleSecondaryColorChange(color, index)}/>
-                    ))}
-                </div>
-                </Form.Item>
-              </Col>
-            </Row>
-          </Form>
-          <Form layout='inline'>
-            <Form.Item label="Style">
-              <Select placeholder="Select a style" onChange={handleStyleChange} value={style}>
-                <Option value="tech">Cinematic</Option>
-                <Option value="nature">Anime</Option>
-                <Option value="business">Photographic</Option>
-                <Option value="business">Fantasy Art</Option>
-                <Option value="business">Digital Art</Option>
-              </Select>
-            </Form.Item>
-          </Form>
-          <Button type='primary' size='large' block onClick={handleGenerate} loading={loading}>Generate</Button>
+          <Button 
+            type='dashed' 
+            size='large' 
+            icon={<UploadOutlined rev={undefined}/>} 
+            onClick={triggerFileInput}
+          >
+            Upload Image
+          </Button>
+        </div>
+        <Form layout="vertical">
+          <Form.Item label="Look & Feel">
+            <TextArea 
+              placeholder="UI that feels fun and exciting..." 
+              value={lookAndFeel}
+              onChange={handleLookAndFeelChange}
+              autoSize={{ minRows: 3, maxRows: 3 }}  // Set min and max rows to 3
+            />
+          </Form.Item>
+        </Form>
+        <Form>
+          <Row 
+            gutter={8} 
+            align="middle" 
+            style={{ alignItems: 'center' }}
+          >
+            <Col 
+              style={{ 
+                display: 'flex', 
+                alignItems: 'center', 
+                justifyContent: 'end', 
+                paddingRight: '8px', 
+                paddingBottom:'10px' 
+              }}
+            >
+              <span>Primary Color</span>
+            </Col>
+            <Col>
+              <Form.Item>
+                <ColorPicker 
+                  trigger="hover" 
+                  placement='bottom'
+                  format='hex' 
+                  defaultValue={primaryColor} 
+                  onChange={(color) => handlePrimaryColorChange(color)}
+                />
+              </Form.Item>
+            </Col>
+          </Row>
+        </Form>
+        <Form>
+          <Row gutter={8} align="middle" style={{ alignItems: 'center' }}>
+            <Col 
+              style={{ 
+                display: 'flex', 
+                alignItems: 'center', 
+                justifyContent: 'end', 
+                paddingRight: '8px', 
+                paddingBottom:'10px' 
+              }}>
+              <span>Secondary Colors</span>
+            </Col>
+            <Col>
+              <Form.Item>
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                {secondaryColors.map((color, index) => (
+                  <ColorPicker 
+                    key={index}
+                    trigger="hover" 
+                    placement='bottom' 
+                    format='hex'
+                    style={{ marginRight: '1px' }} 
+                    defaultValue={color} 
+                    onChange={(color) => handleSecondaryColorChange(color, index)}
+                  />
+                ))}
+              </div>
+              </Form.Item>
+            </Col>
+          </Row>
+        </Form>
+        <Form layout='inline'>
+          <Form.Item label="Style" style={{width: '100%'}}>
+            <Select 
+              placeholder="Select a style" 
+              onChange={handleStyleChange} 
+              value={style}
+              style={{
+                width: '100%'
+              }}
+            >
+              <Option value="Cinematic">Cinematic</Option>
+              <Option value="Anime">Anime</Option>
+              <Option value="Photographic">Photographic</Option>
+              <Option value="Fantasy Art">Fantasy Art</Option>
+              <Option value="Digital Art">Digital Art</Option>
+            </Select>
+          </Form.Item>
+        </Form>
+        <Button 
+          type='primary' 
+          size='large' 
+          block 
+          onClick={handleGenerate} 
+          loading={loading}
+        >
+          Generate
+        </Button>
       </ConfigProvider>      
   );
 }
--- src/plugin/controller.ts
+++ src/plugin/controller.ts
@@ -2,53 +2,21 @@ figma.showUI(__html__);
 figma.ui.resize(300, 500);
 
 figma.ui.onmessage = (msg) => {
-  // function base64ToUint8Array(base64String: string): Uint8Array | null {
-  //   try {
-  //     // Remove the data URL prefix if present
-  //     const base64WithoutPrefix = base64String.replace(/^data:\w+\/\w+;base64,/, '');
-  
-  //     // Decode the base64 string using Buffer
-  //     const buffer = Buffer.from(base64WithoutPrefix, 'base64');
-  
-  //     // Create a Uint8Array from the Buffer
-  //     const uint8Array = new Uint8Array(buffer);
-  
-  //     return uint8Array;
-  //   } catch (error) {
-  //     console.error('Error converting base64 to Uint8Array:', error.message);
-  //     return null;
-  //   }
-  // }
   if (msg.type === 'generated-image') {
-    console.log(msg)
-    const uint8Array = msg.uint8Array
-    const imageNode = figma.createImage(uint8Array)
+    const uint8Array = msg.uint8Array;
+    const imageNode = figma.createImage(uint8Array);
     
-    const node = figma.createRectangle()
-    node.resize(600, 600)
-    // node.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
+    const node = figma.createRectangle();
+    node.resize(600, 600);
     node.fills = [
       {
         type: 'IMAGE',
         imageHash: imageNode.hash,
         scaleMode: 'FILL'
       }
-    ]
-    figma.currentPage.selection = [node]
+    ];
+    figma.currentPage.selection = [node];
     figma.viewport.scrollAndZoomIntoView([node]);
-    // const nodes = [];
-    // for (let i = 0; i < 5; i++) {
-    //   const rect = figma.createRectangle();
-    //   rect.x = i * 150;
-    //   rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
-    //   figma.currentPage.appendChild(rect);
-    //   nodes.push(rect);
-    // }
-
-    // figma.currentPage.selection = nodes;
-    // figma.viewport.scrollAndZoomIntoView(nodes);
-
-    // This is how figma responds back to the ui
     figma.ui.postMessage({
       type: 'generate-image',
       message: `Image Generated`,
